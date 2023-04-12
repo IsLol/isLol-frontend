f7471d25571495a371ebe7f7294dd94d3ff277ed
sajuni11 <sajuni11@dotincorp.com>
2023-04-12 14:15:52 +0900

    반응형 작업

--- components/layout/centerForm.tsx
+++ components/layout/centerForm.tsx
@@ -1,4 +1,3 @@
-import styles from '@/pages/auth/login.module.css';
 import { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -9,10 +8,14 @@ interface LayoutProps {
 
 function CenterForm({ children, className, title }: LayoutProps) {
   return (
-    <div className="container" id={styles.container}>
-      <div className={`${className}_form`} id={styles.loginForm}>
-        <h1 className="text-center mb-5">{title}</h1>
-        {children}
+    <div className="container-fluid px-0 h-100">
+      <div className="row justify-content-center align-items-center no-gutters h-100">
+        <div
+          className={`col-10 col-sm-10 col-md-8 col-lg-6 col-xl-3 ${className}_form`}
+        >
+          <h1 className="text-center mb-5">{title}</h1>
+          {children}
+        </div>
       </div>
     </div>
   );
