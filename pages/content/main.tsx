2d48753945323f95e97e3801293085df0e58f46b
이승원 <skmb1230@naver.com>
2023-05-07 00:18:55 +0900

    Table 컴포넌트 생성 및 메인에 테스트 완료.

--- pages/content/main.tsx
+++ pages/content/main.tsx
@@ -1,5 +1,5 @@
 import CenterForm from '@/components/Layout/CenterForm';
-import { Inputs } from '@components';
+import { Inputs, Table } from '@components';
 import { Button, InputGroup } from 'react-bootstrap';
 
 function Main() {
@@ -14,6 +14,65 @@ function Main() {
   return (
     <div className="container-fluid px-0">
       <h2 className="text-center">검색</h2>
+      <Table
+        columns={[
+          {
+            header: '헤더1',
+            headerId: 'H01',
+            isSortable: true,
+          },
+          {
+            header: '헤더2',
+            headerId: 'H02',
+          },
+          {
+            header: '헤더3',
+            headerId: 'H03',
+          },
+        ]}
+        rows={[
+          {
+            items: [
+              {
+                item: '테스트1',
+              },
+              {
+                item: '테스트1',
+              },
+              {
+                item: '테스트1',
+              },
+            ],
+            rowHighlight: true,
+          },
+          {
+            items: [
+              {
+                item: '테스트2',
+              },
+              {
+                item: '테스트2',
+              },
+              {
+                item: '테스트2',
+              },
+            ],
+          },
+          {
+            items: [
+              {
+                item: '테스트3',
+              },
+              {
+                item: '테스트3',
+              },
+              {
+                item: '테스트3',
+              },
+            ],
+          },
+        ]}
+      />
       <CenterForm className="login" defaultWidth="4">
         <InputGroup>
           <Inputs inputs={inputProps} />
