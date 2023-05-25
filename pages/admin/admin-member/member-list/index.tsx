ade21f1efd7e10ce3b7f30b377102544f6cd2a7e
이승원 <skmb1230@naver.com>
2023-05-25 19:06:31 +0900

    Textfield 컴포넌트 기본작업 * 회원목록 상태관리 추가

--- pages/admin/admin-member/member-list/index.tsx
+++ pages/admin/admin-member/member-list/index.tsx
@@ -4,6 +4,7 @@ import { handleTableRowList } from '@islol-utils';
 import { dehydrate } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
 import { GetServerSideProps } from 'next';
+import { useState } from 'react';
 import style from './index.module.scss';
 
 const COLUMN_LIST: TableColumn[] = [
@@ -45,11 +46,26 @@ const cx = classNames.bind(style);
 const rootClass = 'member-list';
 export function MemberListPage() {
   const { data, refetch, isError, isLoading } = useMemberList();
+  const [isModify, setIsModify] = useState<boolean>(false);
 
   return (
     <div className={cx(rootClass)}>
       <div className={cx(`${rootClass}__button-wrap`)}>
-        <Button color="blue">일괄저장</Button>
+        <Button
+          color={isModify ? 'orange' : 'blue'}
+          onClick={() => {
+            if (isModify) {
+              alert(
+                '일괄수정을 취소 하시겠습니까?\n작성했던 내용은 사라지게 됩니다.'
+              );
+            }
+            setIsModify(!isModify);
+          }}
+        >
+          {isModify ? '수정취소' : '일괄수정'}
+        </Button>
+        {isModify && <Button color="blue">일괄저장</Button>}
+
         <Button>탈퇴</Button>
       </div>
       <Table
@@ -57,6 +73,7 @@ export function MemberListPage() {
         rows={data && handleTableRowList(data, COLUMN_LIST)}
         title="회원 목록"
         rowHeight={70}
+        isModify={isModify}
       />
     </div>
   );
