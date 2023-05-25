5fc5ca9d96ee8b1cff2f324878e0bc87a07aa4a5
이승원 <skmb1230@naver.com>
2023-05-25 11:35:30 +0900

    tableList 공통함수로 경로 이동 * tableRowHover 적용

--- pages/admin/admin-member/member-list/index.tsx
+++ pages/admin/admin-member/member-list/index.tsx
@@ -1,12 +1,11 @@
-import style from './index.module.scss';
-import classNames from 'classnames/bind';
-import { Button, Table, TableColumn } from '@islol-components';
-import { useState } from 'react';
-import { GetServerSideProps } from 'next';
 import { prefetchMemberList, useMemberList } from '@islol-api';
+import { Button, Table, TableColumn } from '@islol-components';
+import { handleTableRowList } from '@islol-utils';
 import { dehydrate } from '@tanstack/react-query';
+import classNames from 'classnames/bind';
+import { GetServerSideProps } from 'next';
+import style from './index.module.scss';
 
-//TODO 임시 데이터
 const COLUMN_LIST: TableColumn[] = [
   {
     header: '계정',
@@ -47,20 +46,6 @@ const rootClass = 'member-list';
 export function MemberListPage() {
   const { data, refetch, isError, isLoading } = useMemberList();
 
-  //table list 핸들링 함수 제네릭은 공통으로 빼기위해 넣어둠
-  const handleTableRowList = <T extends Record<string, any>>() => {
-    const tableRowList = data.map((member: T) => {
-      return {
-        items: COLUMN_LIST.map((column) => {
-          return {
-            item: member[column.headerId],
-          };
-        }),
-      };
-    });
-    return tableRowList;
-  };
-
   return (
     <div className={cx(rootClass)}>
       <div className={cx(`${rootClass}__button-wrap`)}>
@@ -69,9 +54,9 @@ export function MemberListPage() {
       </div>
       <Table
         columns={COLUMN_LIST}
-        rows={data && handleTableRowList()}
+        rows={data && handleTableRowList(data, COLUMN_LIST)}
         title="회원 목록"
-        rowHeight={50}
+        rowHeight={70}
       />
     </div>
   );
---
+++ utils/tableOption/index.tsx
@@ -0,0 +1,17 @@
+import { TableColumn, TableRow } from '@islol-components';
+
+export const handleTableRowList = <T extends Record<string, any>>(
+  DataList: T,
+  columnList: TableColumn[]
+): TableRow[] => {
+  const tableRowList = DataList.map((member: T) => {
+    return {
+      items: columnList.map((column) => {
+        return {
+          item: member[column.headerId],
+        };
+      }),
+    };
+  });
+  return tableRowList;
+};
